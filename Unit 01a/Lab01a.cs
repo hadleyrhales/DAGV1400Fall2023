94a9ee6547ca16192cfccd0f68d9175004a86fe6
Hadley Hales <10878010@uvu.edu>
2023-09-05 14:07:20 -0600

    added comments to code

--- Unit 01a/Lab01a.cs
+++ Unit 01a/Lab01a.cs
@@ -1,15 +1,15 @@
 public class Program
 {
-	public ArmourStats chestPlate;
-	public ArmourUpgrade newUpgrade;
+	public ArmourStats chestPlate; // creating chestPlate object in ArmourStats class
+	public ArmourUpgrade newUpgrade; // creating newUpgrade object in ArmourUpgrade class
 	
 	public void Main()
 	{
-		chestPlate = new ArmourStats();
-		newUpgrade = new ArmourUpgrade();
+		chestPlate = new ArmourStats(); // assigning chestPlate object with values of ArmourStats class
+		newUpgrade = new ArmourUpgrade(); // assigning newUpgrade object with values of ArmourUpgrade class
 		
-		chestPlate.armourEqip = true;
-		chestPlate.armourStrength += newUpgrade.upgradeStrength;
+		chestPlate.armourEqip = true; // Equipping chestPlate
+		chestPlate.armourStrength += newUpgrade.upgradeStrength; // Adding armourStrength and upgradeStrength together for new armourStrength
 		Console.WriteLine(chestPlate.armourPiece + " has been equipped.");
 		Console.WriteLine(chestPlate.armourPiece + " has a strength of " + chestPlate.armourStrength + ".");
 	}
@@ -17,12 +17,12 @@ public class Program
 
 public class ArmourStats
 {
-	public bool armourEqip = false;
-	public int armourStrength = 50;
-	public string armourPiece = "Chestplate";
+	public bool armourEqip = false; // inital equip state
+	public int armourStrength = 50; // initial armourStrength
+	public string armourPiece = "Chestplate"; // armourPiece name
 }
 
 public class ArmourUpgrade
 {
-	public int upgradeStrength = 10;	
+	public int upgradeStrength = 10; // Upgrade value	
 }
